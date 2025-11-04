4339564d6b3a1d68ef893f5ef2eae9f27b896f0e
MoinulIslamNabin <moinul.islam.nabin779@gmail.com>
2025-11-04 22:01:12 +0600

    navbar complete

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,44 @@
+import logo from "../../assets/DtdLogo.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar border-none bg-transparent">
+      <div className="flex navbar-start">
+        <a className="btn btn-ghost rounded-2xl p-0 md:p-2 md:py-8 text-start border hover:shadow-[0_0_15px_#1DA0E3] transition-all duration-300 ">
+          <div className="title flex items-center gap-2">
+            <img src={logo} alt="Dtd Logo" className="md:w-20 w-12 md:h-13 h-8 md:mr-2" />
+            <div>
+              <p className="md:text-2xl text-base text-transparent bg-clip-text bg-linear-to-r from-[#1DA0E3] to-[#0D8ED9]">
+                Dig The Data
+              </p>
+              <p className="md:text-sm text-xs text-[#1DA0E3]">4.0</p>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div className="hidden md:flex navbar-center gap-6">
+        <a className="cursor-pointer outline-none bg-transparent border border-transparent rounded-xl text-base text-white/70 font-medium transition-all duration-300 hover:text-[#1DA0E3] hover:[text-shadow:0_0_10px_#1DA0E3]">
+          Gallery
+        </a>
+
+        <a className="cursor-pointer outline-none bg-transparent border border-transparent rounded-xl text-base text-white/70 font-medium transition-all duration-300 hover:text-[#1DA0E3] hover:[text-shadow:0_0_10px_#1DA0E3]">
+          Legacy
+        </a>
+      </div>
+      <div className="flex gap-2 md:mr-3 navbar-end">
+        <a
+          className="btn btn-ghost md:px-6 md:py-6 bg-transparent border border-[#0D5D88] rounded-xl md:text-lg text-xs text-[#01CDEE] font-semibold hover:border-[#1DA0E3] hover:shadow-[0_0_15px_#1DA0E3] transition-all duration-300"
+        >
+          Login
+        </a>
+        <a
+          className="btn btn-ghost md:px-6 md:py-6 bg-linear-to-l to-[#00B3FF] from-[#0077FF] border border-transparent rounded-xl md:text-lg text-xs text-white font-semibold hover:border-[#1DA0E3] hover:shadow-[0_0_15px_#1DA0E3] transition-all duration-300"
+        >
+          Register Now
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
